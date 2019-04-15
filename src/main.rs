059c091c2187d87e792947e69762992635558f6c
MLA98 <leoyoudatou@outlook.com>
2019-04-14 23:42:40 -0700

    first version withou only tar support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+use structopt::StructOpt;
+use std::fs;
+use std::process::Command;
+
+fn main(){
+    let args = Cli::from_args();
+    if !path_exists(&args.target){
+        panic!("The target file does not exist!")
+    }
+
+    let md = fs::metadata(&args.target).unwrap();
+    if md.is_dir(){
+        let _output = Command::new("tar")
+                    .arg("-cvf")
+                    .arg(format!("{}.tar", &args.target))
+                    .arg(&args.target)
+                    .output()
+                    .expect("failed to execute process");
+    }
+    else{
+        let file_type = Command::new("file")
+                     .arg(&args.target)
+                     .output()
+                     .expect("failed to execute process");
+        if String::from_utf8_lossy(&file_type.stdout).contains("POSIX tar archive"){
+            let _output = Command::new("tar")
+                         .arg("-xvf")
+                         .arg(&args.target)
+                         .output()
+                         .expect("failed to execute process");
+        }
+        else{
+            println!("This is neither a folder or a tar file!")
+        }
+    }
+}
+
+
+
+#[derive(StructOpt)]
+struct Cli {
+    target: String,
+    // #[structopt(parse(from_os_str))]
+    // path: std::path::PathBuf,
+}
+
+pub fn path_exists(path: &str) -> bool {
+    fs::metadata(path).is_ok()
 }
