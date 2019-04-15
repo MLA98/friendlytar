10d30faf78b3e7e9de5f83b0af235ec16d27c3d0
MLA98 <yuldong@ucdavis.edu>
2019-04-15 09:31:30 -0700

    Rust fmt

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,37 @@
-use structopt::StructOpt;
 use std::fs;
 use std::process::Command;
+use structopt::StructOpt;
 
-fn main(){
+fn main() {
     let args = Cli::from_args();
-    if !path_exists(&args.target){
+    if !path_exists(&args.target) {
         panic!("The target file does not exist!")
     }
 
     let md = fs::metadata(&args.target).unwrap();
-    if md.is_dir(){
+    if md.is_dir() {
         let _output = Command::new("tar")
-                    .arg("-cvf")
-                    .arg(format!("{}.tar", &args.target))
-                    .arg(&args.target)
-                    .output()
-                    .expect("failed to execute process");
-    }
-    else{
+            .arg("-cvf")
+            .arg(format!("{}.tar", &args.target))
+            .arg(&args.target)
+            .output()
+            .expect("failed to execute process");
+    } else {
         let file_type = Command::new("file")
-                     .arg(&args.target)
-                     .output()
-                     .expect("failed to execute process");
-        if String::from_utf8_lossy(&file_type.stdout).contains("POSIX tar archive"){
+            .arg(&args.target)
+            .output()
+            .expect("failed to execute process");
+        if String::from_utf8_lossy(&file_type.stdout).contains("POSIX tar archive") {
             let _output = Command::new("tar")
-                         .arg("-xvf")
-                         .arg(&args.target)
-                         .output()
-                         .expect("failed to execute process");
-        }
-        else{
+                .arg("-xvf")
+                .arg(&args.target)
+                .output()
+                .expect("failed to execute process");
+        } else {
             println!("This is neither a folder or a tar file!")
         }
     }
 }
-
-
 
 #[derive(StructOpt)]
 struct Cli {
